324c59591bdb663c00a9b19815a18079aece01ef
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-03-10 18:00:45 +0200

    Added a sign up activity page. Created a simple home page with a button which redirects to the sign up activity.

---
+++ app/src/main/java/com/GroupC/fly/SignUpActivity.java
@@ -0,0 +1,10 @@
+package com.GroupC.fly;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class SignUpActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
