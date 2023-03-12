7d5f6d2f4fe65b5be1fd6eb9694a1f692ddd95bc
Roy Toledano <royto@mta.ac.il>
2023-03-12 16:56:28 +0200

    Getters(email and password), temporary

--- app/src/main/java/com/GroupC/fly/ActivityLogic/SignUpActivity.java
+++ app/src/main/java/com/GroupC/fly/ActivityLogic/SignUpActivity.java
@@ -7,10 +7,18 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.GroupC.fly.R;
 
 public class SignUpActivity extends AppCompatActivity{
+    String email, password;
+    EditText etEmail;
+    EditText etPassword;
+    EditText etPassword_check;
+    Button submitButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +35,27 @@ public class SignUpActivity extends AppCompatActivity{
         animation.setEnterFadeDuration(10);
         animation.setExitFadeDuration(5000);
         animation.start();
+
+        submitButton = (Button) findViewById(R.id.btn_submit);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getEmailAddress();
+                getPassword();
+
+            }
+        });
+    }
+
+    //Getting email address from the EditText.
+    private void getEmailAddress(){
+        etEmail = (EditText) findViewById(R.id.et_email);
+        email = etEmail.getText().toString();
+    }
+    //Getting password address from the EditText.
+    private void getPassword(){
+        etPassword = (EditText) findViewById(R.id.et_password);
+        password = etPassword.getText().toString();
     }
 
     public void onReturnClick(View view) {
