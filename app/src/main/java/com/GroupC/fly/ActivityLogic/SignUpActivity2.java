615ba485c25991d8642cb83068a0305cdafbacc7
BeBo1337 <yoleb123@gmail.com>
2023-03-27 16:03:15 +0300

    - Fixed bug with some edit text in sign up 1 - Added date of birth picker in sign up 2 - Fixed a bug that caused crashing with the question mark in sign up 1

--- app/src/main/java/com/GroupC/fly/ActivityLogic/SignUpActivity2.java
+++ app/src/main/java/com/GroupC/fly/ActivityLogic/SignUpActivity2.java
@@ -1,22 +1,33 @@
 package com.GroupC.fly.ActivityLogic;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.graphics.drawable.AnimationDrawable;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
+
 import com.GroupC.fly.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class SignUpActivity2 extends AppCompatActivity {
 
-    String[] gender_drop_down;
+    String[] genderDropDown;
     AutoCompleteTextView autoCompleteTV;
     ArrayAdapter<String> adapter;
+
+    private DatePickerDialog datePickerDialog;
+
+    private AppCompatButton dateOfBirthButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +39,67 @@ public class SignUpActivity2 extends AppCompatActivity {
 
         //Makes drop down menu of gender work with spam with the design provided
         autoCompleteTV = findViewById(R.id.tv_auto_complete);
-        gender_drop_down= getResources().getStringArray(R.array.genders_options);
-        adapter = new ArrayAdapter<>(getApplicationContext(),R.layout.drop_down,gender_drop_down);
+        genderDropDown = getResources().getStringArray(R.array.genders_options);
+        adapter = new ArrayAdapter<>(getApplicationContext(),R.layout.drop_down, genderDropDown);
         autoCompleteTV.setAdapter(adapter);
 
+        dateOfBirthButton = findViewById(R.id.btn_dob);
+        initDatePicker();
+
+    }
+
+    /**
+     * Creates date picker logic
+     */
+    private void initDatePicker()
+    {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String date = makeDateString(year,month,day);
+                dateOfBirthButton.setText(date);
+            }
+        };
+
+        //Show today's date when starting dialog
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        //builds the dialog
+        datePickerDialog = new DatePickerDialog(this, R.style.DatePickerTheme,
+                dateSetListener,year,month,day);
+        datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+        //this is retarded but they fucked up so I need to add this
+        datePickerDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+    }
+
+    //TODO: Idan's mom as well change the below method to something build in, there must be something
+    private String makeDateString(int year,int month,int day)
+    {
+        String dateFormat="";
+
+        if (day<10 && day > 0)
+            dateFormat += "0";
+        dateFormat += day+"/";
+
+        if(month<10 && month > 0)
+            dateFormat += "0";
+        dateFormat += month+"/";
+
+        dateFormat += year;
+        return dateFormat;
+    }
+    public void selectDateOfBirth(View view) {
+        datePickerDialog.show();
     }
 
     public void onGoBackClick(View view) {
         Intent moveBack = new Intent(this, SignUpActivity.class);
         startActivity(moveBack);
     }
+
 }
