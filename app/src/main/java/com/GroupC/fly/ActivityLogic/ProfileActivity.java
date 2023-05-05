dd795fed0e2e6cacf19dc1f5ceeca2617f468500
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-05-05 20:43:08 +0300

    added basic things to profile activity

--- app/src/main/java/com/GroupC/fly/ActivityLogic/ProfileActivity.java
+++ app/src/main/java/com/GroupC/fly/ActivityLogic/ProfileActivity.java
@@ -1,4 +1,29 @@
 package com.GroupC.fly.ActivityLogic;
 
-public class ProfileActivity {
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.GroupC.fly.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    ImageView ivProfilePicture;
+    TextView tvJob, tvLocation, tvBdate, tvAge, tvName;
+
+    /** METHODS **/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        ivProfilePicture = findViewById(R.id.iv_profile_picture_profile_page);
+        tvJob = findViewById(R.id.tv_job_content);
+        tvLocation = findViewById(R.id.tv_location_content);
+        tvBdate = findViewById(R.id.tv_bdate_content);
+        tvAge = findViewById(R.id.tv_age_content);
+        tvName = findViewById(R.id.tv_profile_name);
+    }
 }
